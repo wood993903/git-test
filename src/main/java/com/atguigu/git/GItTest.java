3442a5f7a86b32b407c739309e2bf0bc3433fa35
wood993903 <30912306@qq.com>
2023-07-07 11:05:49 +0800

    Update GItTest.java
    pull-commit

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -10,5 +10,6 @@ public class GItTest {
         System.out.println("master test");
         System.out.println("hot-fix test1");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
